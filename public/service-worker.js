48e54081c874aab41401cfa1bd71b5701df15ac5
Drugo <d.zago.1991@gmail.com>
2026-02-16 00:30:51 +0100

    fix cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,28 +1,24 @@
 /* eslint-env serviceworker */
 /* eslint-disable no-restricted-globals */
 
-const CACHE_NAME = 'verbale-pallanuoto-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/static/js/main.js',
-  '/static/css/main.css',
-  '/manifest.json',
-  '/logo192.png',
-  '/logo512.png'
-];
+const CACHE_NAME = 'verbale-pallanuoto-v2';
 
-// Installazione - cache dei file essenziali
+// Installazione - cache solo file base
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
         console.log('Cache aperta');
-        return cache.addAll(urlsToCache).catch(err => {
-          console.log('Errore cache iniziale, continuo comunque:', err);
-        });
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/manifest.json',
+          '/logo192.png',
+          '/logo512.png'
+        ]);
       })
       .then(() => self.skipWaiting())
+      .catch(err => console.log('Errore cache:', err))
   );
 });
 
@@ -31,46 +27,40 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => {
             console.log('Elimino vecchia cache:', cacheName);
             return caches.delete(cacheName);
-          }
-        })
+          })
       );
     }).then(() => self.clients.claim())
   );
 });
 
-// Fetch - strategia Cache First, poi Network
+// Fetch - Network first, poi Cache
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
+    fetch(event.request)
       .then((response) => {
-        if (response) {
-          return response;
-        }
-
-        return fetch(event.request)
-          .then((response) => {
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            const responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          })
-          .catch(() => {
-            if (event.request.headers.get('accept').includes('text/html')) {
-              return caches.match('/index.html');
-            }
+        if (response && response.status === 200) {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseClone);
           });
+        }
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request).then((cachedResponse) => {
+          if (cachedResponse) {
+            return cachedResponse;
+          }
+          if (event.request.headers.get('accept').includes('text/html')) {
+            return caches.match('/index.html');
+          }
+          return null;
+        });
       })
   );
 });
